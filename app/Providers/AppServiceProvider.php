4db02e006b222aa01d3d892bad97043e7d913666
nizar-sys <raspbery132@gmail.com>
2021-12-04 21:11:49 +0700

    RPL-Echosystem v1.0

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
@@ -26,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Blade::withoutDoubleEncoding();
-        if (env('APP_ENV') != 'local') {
+        if (App::isProduction()) {
             URL::forceScheme('https');
         }
     }
